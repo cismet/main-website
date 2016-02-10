2871fddca870b7a822f2a4d708407e841e0ed210
helllth <th@cismet.de>
2016-02-10 15:40:01 +0100

    working on #4: middleman 4 version bump

--- config.rb
+++ config.rb
@@ -4,101 +4,48 @@ activate :i18n
 #  localize :path => "/:locale/", :mount_at_root => :de, :langs => [:de, :en]
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true,
-              :autolink => true, 
+              :autolink => true,
                :smartypants => true,
 	       :tables => true
 
-### 
-# Compass
-###
 
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
+         # Per-page layout changes:
+         #
+         # With no layout
+         page '/*.xml', layout: false
+         page '/*.json', layout: false
+         page '/*.txt', layout: false
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+         # With alternative layout
+         # page "/path/to/file.html", layout: :otherlayout
 
-###
-# Haml
-###
+         # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+         # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+         #  which_fake_page: "Rendering a fake page with a local variable" }
 
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-# require 'haml-coderay'
+         # General configuration
 
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-# require 'coffee-filter'
+         # Reload the browser automatically whenever files change
+         configure :development do
+           activate :livereload
+         end
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+         ###
+         # Helpers
+         ###
 
-###
-# Page command
-###
+         # Methods defined in the helpers block are available in templates
+         # helpers do
+         #   def some_helper
+         #     "Helping"
+         #   end
+         # end
 
-# Per-page layout changes:
-# 
-# With no layout
-# page "/path/to/file.html", :layout => false
-# 
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-# 
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+         # Build-specific configuration
+         configure :build do
+           # Minify CSS on build
+           # activate :minify_css
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Change the CSS directory
-# set :css_dir, "alternative_css_directory"
-
-# Change the JS directory
-# set :js_dir, "alternative_js_directory"
-
-# Change the images directory
-# set :images_dir, "alternative_image_directory"
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  activate :minify_css
-  
-  # Minify Javascript on build
-  activate :minify_javascript
-  
-  # Enable cache buster
-  activate :cache_buster
-  
-  # Use relative URLs
-  #activate :relative_assets
-  
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  #activate :i18n
-  #localize
-  
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
-end
+           # Minify Javascript on build
+           # activate :minify_javascript
+         end
