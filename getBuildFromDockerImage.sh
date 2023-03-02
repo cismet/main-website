013fc3691079a480b24b7a823778a2625b37965a
Thorsten Hell <thorsten.hell@cismet.de>
2023-03-02 08:42:08 +0100

    :rocket: deploy to gh-pages

--- getBuildFromDockerImage.sh
+++ getBuildFromDockerImage.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -t --rm -v $(pwd):/tmp cismet/main-website sh -c "cp -rv /usr/share/nginx/html/* /tmp"
+docker run -t --rm -v $(pwd):/tmp cismet/main-website:v23.02.1 sh -c "cp -rv /usr/share/nginx/html/* /tmp"
